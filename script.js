35db6c46ad00c976007058e3572efc8d22a9d9d6
sushil8900 <142325585+sushil8900@users.noreply.github.com>
2023-09-10 16:43:37 +0530

    done fronted

---
+++ script.js
@@ -0,0 +1,44 @@
+// Simulated data for tutors
+const tutors = [
+    { name: "John Doe", topic: "Mathematics", rating: 4.8 },
+    { name: "Jane Smith", topic: "Science", rating: 4.6 },
+    { name: "Alice Johnson", topic: "History", rating: 4.9 },
+    // Add more tutor data here
+];
+
+const searchForm = document.getElementById("search-form");
+const topicInput = document.getElementById("topic");
+const resultsContainer = document.getElementById("results");
+
+searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const searchTerm = topicInput.value.toLowerCase();
+
+    // Filter tutors based on the topic
+    const filteredTutors = tutors.filter((tutor) =>
+        tutor.topic.toLowerCase().includes(searchTerm)
+    );
+
+    displayResults(filteredTutors);
+});
+
+function displayResults(tutors) {
+    resultsContainer.innerHTML = "";
+
+    if (tutors.length === 0) {
+        resultsContainer.innerHTML = "<p>No tutors found.</p>";
+        return;
+    }
+
+    tutors.forEach((tutor) => {
+        const tutorCard = document.createElement("div");
+        tutorCard.classList.add("tutor-card");
+        tutorCard.innerHTML = `
+            <h3>${tutor.name}</h3>
+            <p>Teaches: ${tutor.topic}</p>
+            <p>Rating: ${tutor.rating}</p>
+        `;
+        resultsContainer.appendChild(tutorCard);
+    });
+}
